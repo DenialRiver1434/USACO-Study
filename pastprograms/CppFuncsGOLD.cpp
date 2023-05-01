413a4befb7e2e4b0f67c4f0e75f55b0349231777
Daniel Ge <52391257+S-D-Ge@users.noreply.github.com>
2023-05-01 12:17:03 -0700

    Update CppFuncsGOLD.cpp

--- pastprograms/CppFuncsGOLD.cpp
+++ pastprograms/CppFuncsGOLD.cpp
@@ -224,6 +224,41 @@ template <class T> T kruskal(int N, vector<pair<T, pair<int, int>>> edges) {
 	// -1 if the graph is not connected, otherwise the sum of the edge lengths
 	return (D.size(1) == N ? ans : -1);
 }
+template <class T> class MinSegmentTree {
+  private:
+	/** The operation to use for combining two elements. (Must be associative)
+	 */
+	T comb(T a, T b) { return std::min(a, b); }
+	const T DEFAULT = 1e18;  // Will overflow if T is an int
+
+	vector<T> segtree;
+	int len;
+
+  public:
+	MinSegmentTree(int len) : len(len), segtree(len * 2, DEFAULT) {}
+
+	/** Sets the value at ind to val. */
+	void set(int ind, T val) {
+		assert(0 <= ind && ind < len);
+		ind += len;
+		segtree[ind] = val;
+		for (; ind > 1; ind /= 2) {
+			segtree[ind >> 1] = comb(segtree[ind], segtree[ind ^ 1]);
+		}
+	}
+
+	/** @return the minimum element in the range [start, end) */
+	T range_min(int start, int end) {
+		assert(0 <= start && start < len && 0 < end && end <= len);
+		T sum = DEFAULT;
+		for (start += len, end += len; start < end; start /= 2, end /= 2) {
+			if ((start & 1) != 0) { sum = comb(sum, segtree[start++]); }
+			if ((end & 1) != 0) { sum = comb(sum, segtree[--end]); }
+		}
+		return sum;
+	}
+};
+
 int main () {
 	int N; // number of vertices
 	vector<pair<lll, pair<int, int>>> edges; // weight, start, end
