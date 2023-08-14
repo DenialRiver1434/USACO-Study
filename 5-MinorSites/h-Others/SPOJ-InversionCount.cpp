e43557954429378d95ba37ffd1e1625ecea771dd
Daniel Ge <52391257+DenialRiver1434@users.noreply.github.com>
2023-08-14 16:10:08 -0700

    s

---
+++ 5-MinorSites/h-Others/SPOJ-InversionCount.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+using namespace std;
+#define pb push_back 
+#define is insert
+#define lb lower_bound
+#define ll long long
+#define V vector
+#define MS multiset
+#define PL pair<ll, ll>
+#define F first
+#define S second
+#define PQ priority_queue
+#define f0r(i, begin, end) for (ll i = begin; i < end; i ++) 
+#define For(i, end, begin) for (ll i = end; i > begin; i --) 
+#define all(X) x.begin(), x.end()
+#define INF 1000000000000000000
+#define inf 1000000000
+#define MOD 1000000009
+#define len(x) (ll)x.size()
+#define fileread(file) ifstream fin; fin.open((string)file + ".in"); ofstream fout; fout.open((string)file + ".out")
+#define fastio ios_base::sync_with_stdio(0); cin.tie(nullptr)
+template<typename T> istream& operator>>(istream& in, vector<T>& a) {for(auto &x : a) in >> x; return in;};
+template<typename T> ostream& operator<<(ostream& out, vector<T>& a) {for(auto &x : a) out << x << ' '; return out;};
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using Tree =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void tc () {
+	int N; cin >> N;
+	ll tot = 0;
+	Tree<int> nums;
+	f0r (i, 0, N) {
+		int x; cin >> x;
+		nums.is(x);
+		tot += i - nums.order_of_key(x);
+	}
+	cout << tot << endl;
+}
+
+int main() {
+	int T; cin >> T;
+	while (T --) {
+		tc ();
+	}
+}
