714e8ff84d185017a03b00458b435848c6df8c7f
Daniel Ge <52391257+DenialRiver1434@users.noreply.github.com>
2023-12-15 11:52:45 -0800

    Create 1517A.cpp

---
+++ 2-CF/1517A.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h> 
+using namespace std;
+#define pb push_back 
+#define is insert
+#define ll long long
+#define V vector
+#define MS multiset
+#define PL pair<ll, ll>
+#define F first
+#define S second
+#define PQ priority_queue
+#define f0r(i, begin, finish) for (ll i = begin; i < finish; i ++) 
+#define For(i, finish, begin) for (ll i = finish; i > begin; i --) 
+#define all(x) x.begin(), x.end()
+#define INF 1000000000000000000
+#define inf 1000000000
+#define MOD 1000000007
+#define len(x) (ll)x.size()
+#define fileread(file) ifstream fin; fin.open((string)file + ".in"); ofstream fout; fout.open((string)file + ".out")
+#define fastio ios_base::sync_with_stdio(0); cin.tie(nullptr)
+template<typename T> istream& operator>>(istream& in, V<T>& a) {for(auto &x : a) in >> x; return in;};
+template<typename T> ostream& operator<<(ostream& out, pair<T, T>& a) {out << a.F << ", " << a.S << endl; return out;};
+template<typename T> ostream& operator<<(ostream& out, tuple<T, T, T>& a) {out << get<0>(a) << ", " << get<1>(a) << ", " << get<2>(a) << endl; return out;};
+template<typename T> ostream& operator<<(ostream& out, V<T>& a) {for(auto &x : a) out << x << ' '; return out;};
+ 
+ll T, N;
+ 
+ll tc () {
+    cin >> N;
+    if (N % 2050 != 0) return -1;
+    N /= 2050;
+    ll c = 0;
+    while (N > 0) {
+        c += N % 10;
+        N /= 10;
+    }
+    return c;
+}
+ 
+int main () {
+    cin >> T;
+    while (T --) {
+        cout << tc () << endl;
+    }
+}
